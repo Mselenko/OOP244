e01c70490a536baec86f9d0bb6980710e4f6ccdc
Mselenko <31703143+Mselenko@users.noreply.github.com>
2018-09-13 22:26:38 -0400

    Update Kingdom.cpp

--- WS02/at-home/Kingdom.cpp
+++ WS02/at-home/Kingdom.cpp
@@ -1,26 +1,9 @@
-/***********************************************************
-// OOP244 Workshop 2: Dynamic Memory
-// File Kingdom.cpp
-// Version 1.0
-// Date 2/3/18
-// Student Margarita Selenko
-// Student id 147393169
-// Student mail mselenko@myseneca.ca
-//
-// Revision History
-///////////////////////////////////////////////////////////
-// Name     Date    Reason
-//
-///////////////////////////////////////////////////////////
-***********************************************************/
-
 
 #include <iostream>
 #include "Kingdom.h"
 using namespace std;
-// TODO: the sict namespace
+
 namespace sict {
-	// TODO:definition for display(...)
 	void display(Kingdom &kingdom) {
 		cout << kingdom.m_name << ", population " << kingdom.m_population << endl;
 	}
