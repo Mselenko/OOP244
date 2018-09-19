e9b8646bab8b2aa63a253658909bb0876cd85fa5
Mselenko <31703143+Mselenko@users.noreply.github.com>
2018-09-18 23:39:48 -0400

    Add files via upload

---
+++ WS05/in-lab/Fraction.h
@@ -0,0 +1,30 @@
+// Student Margarita Selenko
+// Student id 147393169
+// Student mail mselenko@myseneca.ca
+
+#ifndef SICT_FRACTION_H
+#define SICT_FRACTION_H
+
+namespace sict {
+	// TODO: define the Fraction class
+	class Fraction {
+		// TODO: declare instance variables
+		int m_numerator;
+		int m_denominator;
+		// TODO: declare private member functions
+		int max() const;
+		int min() const;
+		void reduce();
+		int gcd() const;
+	public:
+		// TODO: declare public member functions
+		Fraction();
+		Fraction(int, int);
+		bool isEmpty() const;
+		void display() const;
+		Fraction operator+(const Fraction& rhs) const;
+		// TODO: declare the + operator overload
+
+	};
+}
+#endif
